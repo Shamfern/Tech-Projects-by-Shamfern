1be3e6e1c17d706b10ebe3420e93a71543ad92c7
Shamila Fernandez <shamilafernandez24@gmail.com>
2025-02-05 16:05:25 -0600

    Add files via upload
    used Java to solved an equation in NetBeans

---
+++ Exercise01_05.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exercise01_05;
+
+/**
+ *
+ * @author Shami
+ */
+public class Exercise01_05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // displaying the equation itself
+        System.out.print("(9.5 * 4.5-2.5 *3)/ (45.5 - 3.5) = ");
+        // displays result of equation
+        System.out.println((9.5 * 4.5 - 2.5 *3) / (45.5 - 3.5));
+    }
+}
+                
